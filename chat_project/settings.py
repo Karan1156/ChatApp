4982b229f4fca9a37e1057051b87ae6353785592
Karan1156 <karansingh81744@gmail.com>
2026-07-05 23:17:56 +0530

    db error resolved

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -102,6 +102,7 @@ WSGI_APPLICATION = 'chat_project.wsgi.application'
 # ============================================
 DATABASES = {
     'default': dj_database_url.config(
+        'DATABASE_URL',
         default='sqlite:///db.sqlite3',
         conn_max_age=600,
         ssl_require=not DEBUG
